adb1827fdeaf02684946d42f5dbea2015de9cee7
dada <davaa4510@gmail.com>
2020-04-18 15:17:11 +0800

    admin template oruulav

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,10 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/template', function(){
+    return view('layouts.layout_master');
+});
